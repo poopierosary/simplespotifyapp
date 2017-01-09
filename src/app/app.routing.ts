791272c4e3de071cda3cd96d17fbb31b455fc29f
poopierosary <kevinsaelee@live.com>
2017-01-09 15:32:36 -0800

    Got rid of 'missing whitespace'  error... Debating to disable it from tslint.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,7 +6,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 
-const appRoutes: Routes = [
-]
+const appRoutes: Routes = [ ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
--- src/app/services/spotify.services.ts
+++ src/app/services/spotify.services.ts
@@ -12,31 +12,31 @@ export class SpotifyServices {
     constructor(private _http:Http) { }
 
 //Query the search url to retrieve the artists
-    searchMusic(str:string, type='artist') {
-        this.searchUrl= "https://apit.spotify.com/v1/search?query="+str+
-            '&offset=0&limit=20type='+type+'&market=US';
+    searchMusic ( str:string, type = 'artist' ) {
+        this.searchUrl = "https://apit.spotify.com/v1/search?query=" + str +
+        '&offset=0&limit=20type=' + type + '&market=US';
          return this._http.get(this.searchUrl)
                          .map(res => res.json());
     }
 
 //Grab the artist(s)
-    getArtist(id:string) {
-        this.artistUrl = 'https://api.sportify.com/v1/artists/'+id;
+    getArtist ( id:string ) {
+        this.artistUrl = 'https://api.sportify.com/v1/artists/' + id;
         return this._http.get(this.artistUrl)
                         .map(res => res.json());
     }
 
 //Grab the list of albums per artist
-    getAlbums(artistId:string) {
-        this.albumsUrl = 'https://api.spotify.com/v1/artists/'+artistId+
+    getAlbums ( artistId:string ) {
+        this.albumsUrl = 'https://api.spotify.com/v1/artists/' + artistId +
             'albums';
         return this._http.get(this.albumsUrl)
                         .map(res => res.json());
     }
 
 //Grab the album information
-    getAlbum(id:string) {
-        this.albumUrl = 'https://api.spotify.com/v1/albums/'+id;
+    getAlbum ( id:string ) {
+        this.albumUrl = 'https://api.spotify.com/v1/albums/' + id;
         return this._http.get(this.albumUrl)
                         .map(res => res.json());
     }
